07696eda83c1aaf39815eb07fb35c6518e97265c
Bryce_Xu <bryce_xu@outlook.com>
2022-11-05 13:45:52 -0400

    initialize project

---
+++ prediction.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def model():
+    st.header("Model training")
+
+def predict():
+    st.header("Predict for a flight")
---
+++ visualization.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def vis_airline_company():
+    st.header("Visualize airline company")
+
+def vis_flight_time():
+    st.header("Visualize flight time")
+
+def vis_flight_distance():
+    st.header("Visualize flight distance")
+
+def vis_flight_destination():
+    st.header("Visualize flight destination")
