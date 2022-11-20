3da2699bd9a3491eca32d5d8e9bfc224675e583c
David Chuan-En Lin <david.lin0424@gmail.com>
2022-11-21 00:43:56 +0800

    Update intro and interface

--- app.py
+++ app.py
@@ -3,18 +3,35 @@ import gdown
 import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
+# from streamlit_extras.switch_page_button import switch_page
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def get_data(filename):
     url = "https://drive.google.com/uc?id=1-4OXefZDioyrobHyhtBfAFMNMem_XmLp"
     output = filename
     gdown.download(url, output, quiet=False)
 
-@st.cache
-def profiler(filename):
-    df = pd.read_csv(filename)
+@st.cache(suppress_st_warning=True)
+def profiler(df):
     pr = df.profile_report()
     st_profile_report(pr)
+
+def switch_page(page_name):
+    from streamlit import _RerunData, _RerunException
+    from streamlit.source_util import get_pages
+    def standardize_name(name: str) -> str:
+        return name.lower().replace("_", " ")
+    page_name = standardize_name(page_name)
+    pages = get_pages("app.py")
+    for page_hash, config in pages.items():
+        if standardize_name(config["page_name"]) == page_name:
+            raise _RerunException(
+                _RerunData(
+                    page_script_hash=page_hash,
+                    page_name=page_name,
+                )
+            )
+    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
 
 import streamlit as st
 import altair as alt
@@ -22,22 +39,48 @@ import pandas as pd
 from constants import delay_category
 
 def intro():
-    st.header("Introduction")
-    st.write("Cancel rate: 9.38 %")
-    st.write("Average delay time: 66.63 minutes")
-    df = pd.DataFrame(
-        {"reasons of delay": list(delay_category.keys()), "value": list(delay_category.values())}
-    )
-    pie_chart = alt.Chart(df).mark_arc().encode(
-        theta=alt.Theta(field="value", type="quantitative"),
-        color=alt.Color(field="reasons of delay", type="nominal"),
-    )
-    st.altair_chart(pie_chart)
+    st.header("Project Flight Never Delay ✈️")
+    st.markdown("As frequent travelers, our team members often experience flight delays and cancellations.\
+        However, there is no good way for us to be informed on whether a flight will be delayed or cancelled in advance.")
+
+    st.markdown("To address our problem, we built a flight delay and cancellation prediction model using previous \
+        flight delay data from the [Bureau of Transportation Statistics - On-Time : Marketing Carrier On-Time Performance dataset](https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGK&QO_fu146_anzr=b0-gvzr). \
+        We further augmented our dataset with fine-grained geographic information by mapping airports to US state \
+        names and coordinates (latitude and longitude) from a [Kaggle dataset](https://www.kaggle.com/datasets/usdot/flight-delays?select=airports.csv).")
+    
+    st.markdown("After data cleaning, we have a dataset of 1,141,693 flight delays from 2021 covering information such as flight time, \
+        flight carrier, flight origin and destinations, flight delay times and reasons, flight cancellations and reasons, and geographical information. You may view \
+        the full dataset at the bottom of the page.")
+    
+    # st.write("Cancel rate: 9.38 %")
+    # st.write("Average delay time: 66.63 minutes")
+    # df = pd.DataFrame(
+    #     {"reasons of delay": list(delay_category.keys()), "value": list(delay_category.values())}
+    # )
+    # pie_chart = alt.Chart(df).mark_arc().encode(
+    #     theta=alt.Theta(field="value", type="quantitative"),
+    #     color=alt.Color(field="reasons of delay", type="nominal"),
+    # )
+    # st.altair_chart(pie_chart)
 
 if __name__ == '__main__':
+    # st.set_page_config(layout="wide")
+    st.set_page_config(page_title='Flight Never Delay', page_icon = '✈️', layout = 'centered', initial_sidebar_state = 'expanded')
+    intro()
+    st.write("Click on one of the following buttons to continue.")
+    if st.button("Predict my flight! 🧠"):
+        switch_page("Prediction")
+    if st.button("Visualize correlations in the data! 📊"):
+        switch_page("Visualization")
+    st.markdown("""---""")
     filename = "data-coordinates.csv"
     get_data(filename)
-    intro()
+    df = pd.read_csv(filename)
+    with st.expander("View full dataset"):
+        st.subheader("Dataset")
+        st.dataframe(df)
+        if st.checkbox("Generate data profile (this takes several minutes as the dataset is large)"):
+            profiler(df)
     
     # nav = st.sidebar.radio("Navigation",
     #                  ("Introduction", "Visualization", "Prediction"))
--- pages/prediction.py
+++ pages/prediction.py
@@ -14,43 +14,44 @@ cancel_lin_reg_model = pickle.load(open(cancel_filename, 'rb'))
 
 
 def pred():
-    st.header("Predict Flight Delay and Cancellation")
-
-    valid_input = True
-    c1, c2 = st.columns(2)
-    with c1:
-        airline = st.selectbox("Airline",
-                                options=[key + ' : ' + value for key, value in airline_name.items()]).split(" : ")[0]
-        date = st.date_input("Departure Date", value = datetime.date.today(), min_value = datetime.datetime(2021, 1, 1), max_value = datetime.datetime(2023, 12, 30))
-    with c2:
-        flight_num = st.text_input('Flight Number', '1111')
-
-    c3, c4 = st.columns(2)
-    with c3:
-        dpt_airport = st.selectbox("From Airport",
-                                options=airports, index = 236)
-        arv_airport = st.selectbox("To Airport",
-                                options=airports, index = 39)
-    with c4:
-        dpt_time = int(st.time_input('Expected Departure Time', datetime.time(13,15)).strftime("%-H%M"))
-        arv_time = int(st.time_input('Expected Arrival Time', datetime.time(15,00)).strftime("%-H%M"))
-        
-
-    if not flight_num.isnumeric():
-        st.write('Please enter a numeric value(0-9).')
-        valid_input = False
-    elif dpt_airport == arv_airport:
-        st.write('Please select different departure and arrival airport.')
-        valid_input = False
-    else:
+    st.header("Flight Delay and Cancellation Prediction")
+    st.markdown("Fill in the information about your flight below and we will predict how long the flight may be \
+         delayed for (or arrive early) and the probability of the flight being cancelled.")
+    with st.form("flight_details"):
         valid_input = True
+        c1, c2 = st.columns(2)
+        with c1:
+            airline = st.selectbox("Airline",
+                                    options=[key + ' : ' + value for key, value in airline_name.items()]).split(" : ")[0]
+            date = st.date_input("Departure Date", value = datetime.date.today(), min_value = datetime.datetime(2021, 1, 1), max_value = datetime.datetime(2023, 12, 30))
+        with c2:
+            flight_num = st.text_input('Flight Number', '1111')
+        c3, c4 = st.columns(2)
+        with c3:
+            dpt_airport = st.selectbox("From Airport",
+                                    options=airports, index = 236)
+            dpt_time = int(st.time_input('Expected Departure Time', datetime.time(13,15)).strftime("%-H%M"))
+        with c4:
+            arv_airport = st.selectbox("To Airport",
+                                    options=airports, index = 39)
+            arv_time = int(st.time_input('Expected Arrival Time', datetime.time(15,00)).strftime("%-H%M"))
 
-    month = date.month
-    day = date.day
-    weekday = date.weekday() + 1
-    
+        if not flight_num.isnumeric():
+            st.write('Please enter a numeric value(0-9).')
+            valid_input = False
+        elif dpt_airport == arv_airport:
+            st.write('Please select different departure and arrival airport.')
+            valid_input = False
+        else:
+            valid_input = True
 
-    if valid_input:
+        month = date.month
+        day = date.day
+        weekday = date.weekday() + 1
+
+        submitted = st.form_submit_button("Predict!")
+
+    if valid_input and submitted:
         dummy_data = [[month, day, weekday, airline, int(flight_num), dpt_airport, arv_airport, dpt_time, arv_time]]
         dummy_df = pd.DataFrame(dummy_data, columns=['MONTH', 'DAY_OF_MONTH', 
                 'DAY_OF_WEEK', 'OP_UNIQUE_CARRIER', 'OP_CARRIER_FL_NUM', 
@@ -60,8 +61,12 @@ def pred():
         delay_pred = delay_lin_reg_model.predict(input)[0]
         cancel_pred = cancel_lin_reg_model.predict_proba(input)[0][1] * 100
         if delay_pred < 0:
-            st.markdown('\n### Your flight will arrive %d minutes early.\n### The probability of cancellation is %.2f%%.' % (delay_pred, cancel_pred))
+            status = "early"
         else:
-            st.markdown('\n### Your flight will arrive %d minutes late.\n### The probability of cancellation is %.2f%%.' % (delay_pred, cancel_pred))
+            status = "delayed"
+        m1, m2 = st.columns(2)
+        m1.metric(label=f"Your flight may be {status} for", value=f"{round(delay_pred)} minutes")
+        m2.metric(label=f"The probability of your flight being cancelled is", value=f"{round(cancel_pred)}%")
+        # st.markdown(f'\n### Your flight will arrive %d minutes {status}.\n### The probability of cancellation is %.2f%%.' % (delay_pred, cancel_pred))
 
 pred()
