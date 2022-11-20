45ee49708246e46c17c00a8ea998cf32d4a42aae
David Chuan-En Lin <david.lin0424@gmail.com>
2022-11-20 14:22:15 +0800

    Get data from gdrive and multipage

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
 import streamlit as st
-from introduction import intro
-from visualization import vis
-from prediction import pred
+import gdown
+import pandas as pd
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+
+@st.cache
+def get_data(filename):
+    url = "https://drive.google.com/uc?id=1-4OXefZDioyrobHyhtBfAFMNMem_XmLp"
+    output = filename
+    gdown.download(url, output, quiet=False)
+
+@st.cache
+def profiler(filename):
+    df = pd.read_csv(filename)
+    pr = df.profile_report()
+    st_profile_report(pr)
+
+import streamlit as st
+import altair as alt
+import pandas as pd
+from constants import delay_category
+
+def intro():
+    st.header("Introduction")
+    st.write("Cancel rate: 9.38 %")
+    st.write("Average delay time: 66.63 minutes")
+    df = pd.DataFrame(
+        {"reasons of delay": list(delay_category.keys()), "value": list(delay_category.values())}
+    )
+    pie_chart = alt.Chart(df).mark_arc().encode(
+        theta=alt.Theta(field="value", type="quantitative"),
+        color=alt.Color(field="reasons of delay", type="nominal"),
+    )
+    st.altair_chart(pie_chart)
 
 if __name__ == '__main__':
-    nav = st.sidebar.radio("Navigation",
-                     ("Introduction", "Visualization", "Prediction"))
-    if nav == "Introduction":
-        intro()
-    elif nav == "Visualization":
-        vis()
-    else:
-        pred()
+    filename = "data-coordinates.csv"
+    get_data(filename)
+    intro()
+    
+    # nav = st.sidebar.radio("Navigation",
+    #                  ("Introduction", "Visualization", "Prediction"))
+    # if nav == "Introduction":
+    #     intro()
+    # elif nav == "Visualization":
+    #     vis()
+    # else:
+    #     pred()
+
+    # profiler(filename)
